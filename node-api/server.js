fc5b3cbeb3140ebbe5a7ecd82e932a98e856a33c
Matheus Benka <matheus.h.benka@gmail.com>
2019-07-28 22:43:52 -0300

    ajuste post usuario

--- node-api/server.js
+++ node-api/server.js
@@ -1,17 +1,6 @@
 const express = require('express')
 const app = express();
 
-app.listen(3001);
-
-app.use(function (req, res, next) {
-    res.header('Content-Type', 'application/json');
-    next();
-});
-
-app.get('/',(req,res)=>{    
-    res.send('Hello joao');
-});
-
 var usuarios = [
     {
         'nome':'joao',
@@ -25,10 +14,35 @@ var usuarios = [
     }
 ]
 
-app.get('/usuarios',(req,res)=>{
-    res.json(usuarios)
+app.use(express.json())
+app.listen(3001);
+
+app.use(function (req, res, next) {    
+    res.header('Content-Type', 'application/json');
+    next();
 });
 
-app.get('/usuario',(req, res)=>{
-    res.json(usuarios[0])
-});
+app.route('/')
+    .get((req,res)=>{    
+        res.send('Hello joao');
+    });
+
+app.route('/usuario')
+    .get((req,res)=>{        
+        res.json(usuarios[0])
+    })
+    .post((req,res)=>{        
+        if(req.body === undefined){            
+            res.send('impossivel continuar com body vazio');            
+        }             
+        usuarios.push(req.body)      
+        res.json({'Mensagem':'Usuario adicionado com sucesso', 'Usuario':req.body})
+    })
+    .delete((req,res)=>{
+        res.send('deletado')
+    });
+
+app.route('/usuarios')
+    .get((req,res)=>{
+        res.json(usuarios)
+    })
